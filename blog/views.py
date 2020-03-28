52dc158baf77b2459368d8b9bbbdebb1eff664ab
Hasan Karadeniz <hsnkrdnz57@gmail.com>
2020-03-28 03:08:29 +0100

    database created, register implemented, footer implemented

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-
-posts = [
-    {
-        'author': 'HKK',
-        'title': 'Blog Post #1',
-        'content': 'First Blog Post',
-        'date_posted': 'March 3, 2020'
-    },
-    {
-        'author': 'BK',
-        'title': 'Blog Post #2',
-        'content': 'Second Blog Post',
-        'date_posted': 'March 3, 2020'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
